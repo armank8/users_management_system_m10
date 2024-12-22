d582ec2bd0b6ca6a3c1b65f8079ac0f6a0d513d6
Arman-Kabir <armankabir8@gmail.com>
2024-12-22 23:51:14 +0600

    server created and data and client cleanup

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+
+const users = [
+  { id: 1, name: "hritik", email: "hritik@gmail.com" },
+  { id: 2, name: "shahrukh", email: "shahrukh@gmail.com" },
+  { id: 3, name: "salman", email: "salman@gmail.com" },
+];
+
+app.get("/", (req, res) => {
+  res.send("Users management server is running");
+});
+
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
