bf9ae77069201c411dba48696a8fbd9ce65064c5
Arman-Kabir <armankabir8@gmail.com>
2024-12-23 00:02:27 +0600

    Tailwind router added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1 className="text-center">Users Management System</h1>
+      
+    </>
+  );
 }
 
 export default App;
