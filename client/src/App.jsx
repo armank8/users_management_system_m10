31f9fa3e23d1f059e2c44b1b7c9885984a8f7c75
Arman-Kabir <armankabir8@gmail.com>
2024-12-23 01:12:49 +0600

    Daisy ui and form created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,42 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
   return (
-    <>
-      <h1 className="text-center">Users Management System</h1>
-      
-    </>
+    <div className="flex flex-col items-center gap-5">
+      <div>
+        <h1 className="text-center">Users Management System</h1>
+        <h3 className="text-center">Number of Users {users.length} </h3>
+      </div>
+
+      {/* Form  onSubmit={}*/}
+      <div>
+        <form className="outline-none">
+          <input type="text" placeholder="name" name="name" id="" />
+          <br />
+          <input type="text" placeholder="email" name="email" id="" />
+          <br />
+          <input type="submit" value="Add User" className="btn btn-primary" />
+        </form>
+      </div>
+
+      {/* Data */}
+      <div className="flex flex-col items-center bg-slate-400 p-4">
+        {users.map((user) => (
+          <p key={user.id}>
+            {user.id}. {user.name} = {user.email}
+          </p>
+        ))}
+      </div>
+    </div>
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+
+// middlewares
+app.use(cors());
 
 const users = [
   { id: 1, name: "hritik", email: "hritik@gmail.com" },
